8f6f978dab65fa71cbb60e0d4cdc7b1bd683d6c3
Tatiana <tanya_potapenko@bigmir.net>
2024-06-10 17:26:15 +0200

    make build

---
+++ index.04a5943d.js
@@ -0,0 +1,2 @@
+!function(e,l){for(var n=document.getElementById(e).rows,o=0;o<n.length;o++){var t=n[o].cells[1].cloneNode(!0),c=n[o].cells.length-1;n[o].insertBefore(t,n[o].cells[c])}}("myTable",0);
+//# sourceMappingURL=index.04a5943d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+function copyTableColumn(tableId, colIndex) {
+    const table = document.getElementById(tableId);
+    const rows = table.rows;
+    for(let i = 0; i < rows.length; i++){
+        const cell = rows[i].cells[colIndex];
+        const clone = cell.cloneNode(true);
+        const lastCellIndex = rows[i].cells.length - 1;
+        rows[i].insertBefore(clone, rows[i].cells[lastCellIndex]);
+    }
+}
+copyTableColumn("myTable", 1);
+
+//# sourceMappingURL=index.f75de5e1.js.map
